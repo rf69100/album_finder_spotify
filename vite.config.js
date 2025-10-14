a85c2e8e901e82bbf1f5168ac6bd5b70ccfc456c
rf69100 <ryanfonseca@DESKTOP-LV2C8DO.localdomain>
2025-10-14 12:25:34 +0200

    version fonctionnelle

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/spotify-finder/', // Important pour le déploiement sur sous-dossier
 })
